a730457084e8c1113b16daea1aa7ac84a18b6530
Jason Prothero <jason@proworks.com>
2025-04-10 15:17:59 -0700

    changes to v15, attempt to make gitignore work

--- src/TinyMCE.Umbraco.Premium.Client/assets/src/index.ts
+++ src/TinyMCE.Umbraco.Premium.Client/assets/src/index.ts
@@ -1,12 +1,9 @@
 import { UmbEntryPointOnInit } from '@umbraco-cms/backoffice/extension-api';
-import { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 // load up the manifests here.
-import { manifests as dashboardManifests } from './dashboards/manifest.ts';
 import { manifests as tinymcepremiumManifests } from './packages/tiny-mce/manifests.ts';
 
-const manifests: Array<ManifestTypes> = [
-    ...dashboardManifests,
+const manifests: Array<UmbExtensionManifest> = [
     ...tinymcepremiumManifests
 ];
 
